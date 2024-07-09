f58a48b2fba0120ed5da9507559ff1191b9b9afb
khalifa-pendrops <dikee5200@gmail.com>
2024-07-09 11:44:57 +0100

    same info

--- src/Components/ProductInfo.jsx
+++ src/Components/ProductInfo.jsx
@@ -15,7 +15,9 @@ function ProductInfo() {
       <Header />
       <div className="flex my-[2rem] justify-around        max-sm:flex-col max-sm:justify-center max-sm:items-center max-sm:gap-6">
         <div className="left flex flex-col gap-4 px-4      max-sm:items-center">
-          <h1 className="text-[2rem] font-bold">Product details</h1>
+          <h1 className="text-[2rem] font-bold       max-sm:text-[1.6rem]">
+            Product details
+          </h1>
           <div className="images flex flex-col gap-6">
             <img className="w-[429px]" src={image6} alt="perfume" />
             <div className="jointimages flex gap-4       max-sm:hidden">
@@ -40,31 +42,37 @@ function ProductInfo() {
         <div className="right bg-custBG p-8 flex flex-col gap-8 h-fit">
           <div className="top flex flex-col gap-6">
             <div className="flex justify-between">
-              <h1 className="font-bold text-xl">Dior Sausage</h1>
+              <h1 className="font-bold text-xl        max-sm:text-[15px]">
+                Dior Sausage
+              </h1>
               <div className="flex items-center gap-2">
                 <img
-                  className="cursor-pointer w-[24px]"
+                  className="cursor-pointer w-[24px]      max-sm:w-[18px]"
                   src={heart}
                   alt="favorite"
                 />
                 <img
-                  className="cursor-pointer w-[24px]"
+                  className="cursor-pointer w-[24px]       max-sm:w-[18px]"
                   src={boxarrow}
                   alt="save"
                 />
               </div>
             </div>
-            <div className="flex gap-4">
-              <img className="w-[151px]" src={image6} alt="perfume" />
+            <div className="flex gap-4      max-sm:items-center max-sm:gap-0">
+              <img
+                className="w-[151px]     max-sm:w-[123px]"
+                src={image6}
+                alt="perfume"
+              />
               <div className="flex flex-col gap-4">
-                <p className="text-[18px] w-[14rem]">
+                <p className="text-[18px] w-[14rem]     max-sm:text-[12px] max-sm:w-[9rem]">
                   Fragrance: Dior Sauvage Type: Eau de Toilette
                 </p>
                 <p className="text-[18px] font-bold">&#36;99.10</p>
               </div>
               <div className="flex flex-col gap-2">
-                <p className="text-[18px]">Quantity</p>
-                <div className="flex text-[18px] font-bold items-center justify-center gap-2 border-2 cursor-pointer border-navYellow">
+                <p className="text-[18px]     max-sm:text-[12px]">Quantity</p>
+                <div className="flex text-[18px] font-bold items-center justify-center gap-2 border-2 cursor-pointer border-navYellow      max-sm:font-normal max-sm:px-1">
                   <p>-</p>
                   <p>1</p>
                   <p>+</p>
@@ -73,14 +81,18 @@ function ProductInfo() {
             </div>
           </div>
           <button
-            className="bg-navYellow text-[20px] font-bold text-black w-[100%]"
+            className="bg-navYellow text-[20px] font-bold text-black w-[100%]      max-sm:text-[14px] max-sm:py-1"
             type="submit"
           >
             Add to Cart
           </button>
           <div className="down flex justify-between">
-            <p className="text-[14px]">Free standard shipping</p>
-            <p className="underline text-[14px] cursor-pointer">Free Returns</p>
+            <p className="text-[14px]      max-sm:text-[12px]">
+              Free standard shipping
+            </p>
+            <p className="underline text-[14px] cursor-pointer     max-sm:text-[12px]">
+              Free Returns
+            </p>
           </div>
         </div>
       </div>
