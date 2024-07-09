003cb44c1d715afe25c92fa6e92c19105830ef15
khalifa-pendrops <dikee5200@gmail.com>
2024-07-09 10:13:44 +0100

    summary ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,26 @@ import Header from "./Components/Header";
 import ProductList from "./Components/ProductList";
 import Footer from "./Components/Footer";
 
+
+
+
+import ProductInfo from "./Components/ProductInfo.jsx";
+import SummaryPage from "./Components/SummaryPage.jsx";
+import Checkout from "./Components/Checkout.jsx";
+import ShippingPage from "./Components/ShippingPage.jsx";
+import Payment from "./Components/Payment.jsx";
+
 const App = () => {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar />
       <Header />
       <ProductList />
-      <Footer />
+      <Footer /> */}
       <Outlet />
+      <ProductInfo />
+      <SummaryPage />
+      
     </div>
   );
 };
--- src/Components/SummaryPage.jsx
+++ src/Components/SummaryPage.jsx
@@ -9,113 +9,119 @@ function SummaryPage() {
   return (
     <div>
       <NavBar />
-      <div>
-        <div className="flex justify-center items-center gap-[4rem] my-[4rem] max-sm:flex-col max-sm:gap-[.2rem] max-sm:my-[1rem]">
-          <div className="flex flex-col gap-[8rem] max-sm:justify-center max-sm:items-center">
-            <div className="flex flex-col justify-between gap-4 max-sm:items-center max-sm:w-[90%] max-sm:gap-6">
-              <div className=" flex flex-col gap-4 max-sm:justify-center ">
-                <h3 className="text-2xl ">Your cart</h3>
-                {/* <div className="hidden max-sm:flex max-sm:justify-between max-sm:border-b max-sm:pb-4 max-sm:border-gray-600 max-sm:w-[70%]">
-                  <p>PRODUCT</p>
-                  <p>TOTAL</p>
-                </div> */}
-                <p className="flex gap-1">
-                  Not ready to checkout?
-                  <Link to="/shop">
-                    <span className="text-blue-400 cursor-pointer">
-                      Continue Shopping
-                    </span>
-                  </Link>
-                </p>
-              </div>
-              <div className="flex gap-[14.5rem] border-b-2  border-horizontalLine pb-8 max-sm:w-[70%] max-sm:border-none">
-                <div className="flex gap-[4rem]">
-                  <div className="border p-2 border-navYellow w-fit max-sm:w-[100px] max-sm:height-[100px] max-sm:border-none">
-                    <img
-                      className="max-sm:w-[80px]"
-                      src={image6}
-                      alt="perfume"
-                      width="100px"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-2 ">
-                    <h5 className="font-semibold text-nowrap">Dior Sausage</h5>
-                    <p className="text-[.7rem]">Quantity: 1</p>
-                    <h5 className="font-semibold">&#36;99.10</h5>
-                    <p className="max-sm:flex max-sm:underline max-sm:cursor-pointer hidden">
-                      Remove
+      <div className="flex justify-around items-center my-[2rem] font-[Outfit]      max-sm:flex-col max-sm:gap-[2rem]">
+        <div className="left flex flex-col gap-[4rem]">
+          <div className="top px-2 flex flex-col gap-4      max-sm:gap-1-0">
+            <h1 className="text-[36px]      max-sm:text-[20px]">Your cart</h1>
+            <p className="text-[16px]     max-sm:hidden">
+              Not ready to checkout?{" "}
+              <span className="text-red-800 cursor-pointer">
+                Continue Shopping
+              </span>
+            </p>
+            <div className="hidden max-sm:Producttotal max-sm:flex max-sm:items-center max-sm:gap-[14rem] max-sm:border     ">
+              <h6 className="max-sm:text-[12px]">PRODUCT</h6>
+              <h6 className="max-sm:text-[12px]">TOTAL</h6>
+            </div>
+            <div>
+              <div className="dior flex gap-[8rem]      max-sm:gap-6">
+                <img
+                  className="border border-navYellow w-[130px]"
+                  src={image6}
+                  alt="perfume"
+                />
+                <div className="rightside flex flex-col gap-4">
+                  <div className="top flex flex-col gap-4">
+                    <p className="font-semibold text-[22px]      max-sm:text-[12px]">
+                      Dior Sausage
+                    </p>
+                    <p className="text-sm text-[14px]     max-sm:text-[10px]">
+                      Quantity: 1
                     </p>
                   </div>
+                  <div className="bottom flex gap-[8rem]     max-sm:gap-[6rem]">
+                    <div className="max-sm:flex max-sm:gap-2">
+                      {" "}
+                      <div className="max-sm:flex max-sm:border max-sm:px-4 max-sm:gap-2 max-sm:items-center max-sm:font-semibold max-sm:border-navYellow">
+                        <h6 className="font-semibold text-[22px]      max-sm:hidden">
+                          &#36;99.10
+                        </h6>
+                        <p>-</p>
+                        <p>1</p>
+                        <p>+</p>
+                      </div>
+                      <img
+                        className="cursor-pointer"
+                        src={union}
+                        alt="delete"
+                      ></img>
+                    </div>
+                    <p className="underline text-[20px] cursor-pointer     max-sm:hidden">
+                      Remove
+                    </p>
+                    <h6 className="hidden max-sm:flex font-semibold">
+                      &#36;99.10
+                    </h6>
+                  </div>
                 </div>
-                <div className="flex flex-col self-end items-end gap-1 my-[4rem]">
-                  <p className="underline cursor-pointer max-sm:hidden">
-                    Remove
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-2 border-2 border-dashed p-4 max-sm:hidden">
-              <div className=" flex justify-between text-horizontalLine border-b border-horizontalLine pb-4">
-                <h6>Return Policy</h6>
-                <Link to="/policy">
-                  <p className="font-semibold">+</p>
-                </Link>
-              </div>
-              <div className="flex flex-col text-horizontalLine border-b border-horizontalLine pb-4">
-                <div className=" flex justify-between pb-4">
-                  <h6>Return Policy</h6>
-                  <Link to="/policy">
-                    <p className="font-semibold">-</p>
-                  </Link>
-                </div>
-                <p className="text-horizontalLine">
-                  You have 30 days from the date of delivery to return your
-                  item.
-                </p>
               </div>
             </div>
           </div>
-
-          <div className="bg-customBGLight p-[3rem] flex flex-col gap-10 w-[20rem] max-sm:bg-white max-sm:w-[80%] max-sm:py-[0] max-sm:justify-center max-sm:items-center">
-            <h6 className="font-semibold max-sm:hidden">Order Summary</h6>
-            <div className="flex flex-col gap-6 max-sm:w-[100%]">
-              <input
-                className="bg-customBGLight pl-2 py-2 border border-black placeholder-pxl"
-                type="text"
-                placeholder="Enter coupon code here"
-              ></input>
-              <button
-                className="bg-black text-white py-2 cursor-pointer font-semibold max-sm:hidden"
-                type="submit"
-              >
-                Apply Promo code
-              </button>
+          <div className="bottom flex flex-col gap-4 border-2 border-dashed roundedsm p-4 text-horizontalLine font-[Montserrat]      max-sm:hidden">
+            <div className="flex justify-between">
+              <h6 className="text-[16px] font-semibold">Return Policy</h6>
+              <p className="cursor-pointer">+</p>
             </div>
-            <div className=" flex flex-col gap-4 max-sm:w-[100%]">
-              <div className="flex justify-between items-center">
-                <p className="text-[.6rem]">Subtotal</p>
-                <p className="font-semibold">&#36;99.10</p>
+            <div>
+              <div className="flex justify-between">
+                <h6 className="text-[16px] font-semibold">Return Policy</h6>
+                <p className="cursor-pointer">-</p>
               </div>
-              <div className="flex justify-between items-center">
-                <p className="text-[.6rem]">Shipping</p>
-                <p className="text-[.8rem]">Calculated at the next step</p>
-              </div>
-              <hr className="border-1 border-horizontalLine"></hr>
-              <div className="flex justify-between items-center">
-                <p className="font-semibold">Total</p>
-                <p className="font-semibold">&#36;99.10</p>
-              </div>
+              <p className="text-[16px]">
+                You have 30 days from the date of delivery to return your
+                item(s).
+              </p>
             </div>
-          <Link to="/checkout">
-              <button
-                className="bg-navYellow py-2 px-8 font-semibold"
-                type="submit"
-              >
-                Continue to checkout
-              </button>
-            </Link>
           </div>
+        </div>
+        <div className="right flex flex-col bg-customeBG justify-center p-6 gap-8     max-sm:px-[4rem]">
+          <h3 className="font-semibold text-[22px]     max-sm:hidden">
+            Order Summary
+          </h3>
+          <input
+            className="px-8 py-2 border-0 border-gray placeholder-pxl"
+            type="text"
+            placeholder="Enter coupon code here"
+          ></input>
+          <button
+            className="px-8 py-2 text-white bg-black font-bold text-[24px]     max-sm:text-[16px]"
+            type="submit"
+          >
+            Apply Promo code
+          </button>
+          <div className="calculate flex flex-col gap-2">
+            <div className="flex justify-between">
+              <p className="text-[14px]      max-sm:text-[12px]">Subtotal</p>
+              <p className="font-bold      max-sm:text-[12px]">&#36;99.10</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-[14px]       max-sm:text-[12px]">Shipping</p>
+              <p className="max-sm:text-[12px]">Calculated at the next step</p>
+            </div>
+            <hr className="border-1 border-gray-400"></hr>
+            <div className="flex justify-between">
+              <p className="text-[20px] font-bold     max-sm:text-[12px]">
+                Total
+              </p>
+              <p className="font-bold    max-sm:text-[12px]">&#36;99.10</p>
+            </div>
+          </div>
+          <button
+            className="px-8 py-2 font-bold text-[24px] bg-navYellow     max-sm:text-[16px]"
+            type="submit"
+          >
+            Continue to checkout
+          </button>
         </div>
       </div>
       <Footer />
